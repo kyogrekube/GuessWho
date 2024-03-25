b45a375fa99e6c7ab35d45acd2c26cf6d1256dcc
kyogrekube <alexblitchfield@gmail.com>
2024-03-25 02:59:58 -0400

    added animation for htting guess and add buttons

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ tiny_font = pygame.font.Font("freesansbold.ttf", 16)
 fps = 60
 timer = pygame.time.Clock()
 
-def generate_images():
+def generate_images(p1_selected_face, p2_selected_face):
     #Finds current working directory and images to use in game
     current_directory = os.getcwd()
     img_directory = current_directory + "\\" + "faces"
@@ -39,12 +39,16 @@ def generate_images():
     random.shuffle(img_list)
     p1_guessed_faces = []
     p2_guessed_faces = []
-    #for i in img_list:
-        #p1_guessed_faces.append([i, False]) #First item in the inner list of p1 and p2 are the image, the second is the boolean if it was guessed or not
-        #p2_guessed_faces.append([i, False])
+    for i in img_list:
+        p1_guessed_faces.append([i, False]) #First item in the inner list of p1 and p2 are the image, the second is the boolean if it was guessed or not
+        p2_guessed_faces.append([i, False])
+    
+    #Selecting random face for each player
+    p1_selected_face = random.choice(img_list)
+    img_list.remove(p1_selected_face)
+    p2_selected_face = random.choice(img_list)
 
-
-    return img_directory, img_list
+    return img_directory, p1_guessed_faces, p2_guessed_faces, p1_selected_face, p2_selected_face
 
 def toggle_names_button_creation(toggle_names_condition):
     if toggle_names_condition == True:
@@ -56,8 +60,29 @@ def toggle_names_button_creation(toggle_names_condition):
 
     return toggle_names_button
 
+def ask_button_creation(ask_condition):
+    if ask_condition == True:
+        ask_button = pygame.draw.rect(screen, dark_gray, [280, HEIGHT-90, 125, 75], 0, 8)
+    else:
+        ask_button = pygame.draw.rect(screen, gray, [280, HEIGHT-90, 125, 75], 0, 8)
+    title_text = title_font.render("Ask", True, white)
+    screen.blit(title_text, (290, 670))
 
-def draw_backgrounds():
+    return ask_button
+
+def guess_button_creation(guess_condition):
+    if guess_condition == True:
+        guess_button = pygame.draw.rect(screen, dark_gray, [435, HEIGHT-90, 195, 75], 0, 8)
+    else:
+        guess_button = pygame.draw.rect(screen, gray, [435, HEIGHT-90, 195, 75], 0, 8)
+    title_text = title_font.render("Guess", True, white)
+    screen.blit(title_text, (445, 670))
+
+    return guess_button
+
+
+
+def draw_backgrounds(current_player, toggle_names_condition, ask_condition, guess_condition):
     #top_menu = pygame.draw.rect(screen, black, [0, 0, WIDTH, 100], 10) #makes hollow white square with black border w thickness based on last param
     #top_menu = pygame.draw.rect(screen, black, [0, 0, WIDTH, 100], 2, 100) #Last param changes border radius
 
@@ -65,35 +90,32 @@ def draw_backgrounds():
     top_menu = pygame.draw.rect(screen, black, [0, 0, WIDTH, 100], 0) #(top x, top y, bottom x, bottom y)
     title_text = title_font.render("Fiji Guess Who", True, white)
     screen.blit(title_text, (450, 20))
-    board_space=pygame.draw.rect(screen, maroon, [0, 100, WIDTH, HEIGHT-200], 0) 
     bottom_menu = pygame.draw.rect(screen, black, [0, HEIGHT-100, WIDTH, 100], 0)
     #restart button
     restart_button = pygame.draw.rect(screen, gray, [30, HEIGHT-90, 220, 75], 0, 8)
     title_text = title_font.render("Restart", True, white)
     screen.blit(title_text, (40, 670))
     #Ask button
-    ask_button = pygame.draw.rect(screen, gray, [280, HEIGHT-90, 125, 75], 0, 8)
-    title_text = title_font.render("Ask", True, white)
-    screen.blit(title_text, (290, 670))
+    ask_button = ask_button_creation(ask_condition)
     #guess button
-    guess_button = pygame.draw.rect(screen, gray, [435, HEIGHT-90, 195, 75], 0, 8)
-    title_text = title_font.render("Guess", True, white)
-    screen.blit(title_text, (445, 670))
+    guess_button = guess_button_creation(guess_condition)
     #toggle names button
     toggle_names_button = toggle_names_button_creation(toggle_names_condition)
     #player label (shows which player's data is on the board)
     curr_player_text = small_font.render("Current Player:", True, white)
     screen.blit(curr_player_text, (1090, 670))
     if current_player == 1:
+        board_space=pygame.draw.rect(screen, maroon, [0, 100, WIDTH, HEIGHT-200], 0) 
         curr_player_text = small_font.render(f"Player {current_player}", True, red)
     else:
+        board_space=pygame.draw.rect(screen, navy, [0, 100, WIDTH, HEIGHT-200], 0) 
         curr_player_text = small_font.render(f"Player {current_player}", True, blue)
     screen.blit(curr_player_text, (1140, 705))
 
     return restart_button, ask_button, guess_button, toggle_names_button
 
 
-def draw_board(rows, cols, img_directory, img_list, first_generation, toggle_names_condition):
+def draw_board(rows, cols, img_directory, guessed_faces, first_generation, toggle_names_condition, current_player):
     #ddraws the 40 white squares
     board_list = []
     """for i in range(rows):
@@ -104,16 +126,19 @@ def draw_board(rows, cols, img_directory, img_list, first_generation, toggle_nam
     img_counter = 0
     for i in range(rows):
         for j in range(cols):
-            if img_counter < len(img_list):
+            if img_counter < len(guessed_faces):
                 piece = pygame.draw.rect(screen, white, [20 + j * 130, (i+1) *130, 100, 100], 0, 4) # (top x, top y, bottom x, bottom y)
-                imp = pygame.image.load(img_directory+"\\"+img_list[img_counter])
+                imp = pygame.image.load(img_directory+"\\"+guessed_faces[img_counter][0])
                 scaled_image = pygame.transform.scale(imp, (100, 100))
                 screen.blit(scaled_image, (20 + j * 130, (i+1) *130))
                 board_list.append(piece)
                 if toggle_names_condition == True:
-                    file_name_list = img_list[img_counter].split(".")
+                    file_name_list = guessed_faces[img_counter][0].split(".")
                     face_name = file_name_list[0][:12]
-                    period3 = tiny_font.render(f"{face_name}", True, black)
+                    if current_player == 1:
+                        period3 = tiny_font.render(f"{face_name}", True, black)
+                    else:
+                        period3 = tiny_font.render(f"{face_name}", True, white)
                     #period3 = tiny_font.render("abcdefghijkl", True, black) #12 letters -> 98 pixels _> 8 pix per -> 4 pix per side
                     #screen.blit(period3, (20 + j * 130, (i+1) *130 + 105))
                     text_rect = period3.get_rect(center=(21 + j * 130+50, (i+1) *130 + 105+10))
@@ -129,10 +154,40 @@ def draw_board(rows, cols, img_directory, img_list, first_generation, toggle_nam
 
     return board_list
 
+def restart_button_logic(first_generation, new_board):
+    first_generation = True
+    new_board = True
+    ###
+    restart_button = pygame.draw.rect(screen, dark_gray, [30, HEIGHT-90, 220, 75], 0, 8)
+    title_text = title_font.render("Restart", True, white)
+    screen.blit(title_text, (40, 670))
+    pygame.display.flip()
+    pygame.time.wait(500)
+    black_screen=pygame.draw.rect(screen, black, [0, 100, WIDTH, HEIGHT], 0) 
+    title_text = title_font.render("Restarting", True, red)
+    screen.blit(title_text, (500, 350))
+    pygame.display.flip()
+    pygame.time.wait(500)
+    period1 = title_font.render(".", True, red)
+    screen.blit(period1, (800, 350))
+    pygame.display.flip()
+    pygame.time.wait(1000)
+    period2 = title_font.render(".", True, red)
+    screen.blit(period1, (820, 350))
+    pygame.display.flip()
+    pygame.time.wait(1000)
+    period3 = title_font.render(".", True, red)
+    screen.blit(period3, (840, 350))
+    pygame.display.flip()
+    pygame.time.wait(1000)
+    pygame.display.flip() #may not be needed
+
+    return first_generation, new_board
+
 rows = 4
 cols = 10
 new_board = True
-current_player = 1 #this is either 1 or 2
+current_player = 0 #this is either 1 or 2 after being set
 p1_selected_face = ""
 p2_selected_face = ""
 p1_guessed_faces = []
@@ -143,19 +198,24 @@ screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Fiji Guess Who")
 
 running = True
-img_list = []
 img_directory = ""
 restart_screen = True
 first_generation = False
 #WWWWWWWWWWWWWWW make firstgeneration True again
 toggle_names_condition = False
+selection_screen = True
+ask_condition = False
+guess_condition = False
+border_cords = []
+border_condition = False
 
 while running:
     timer.tick(fps)
     screen.fill(white)
     if new_board == True:
-        img_directory, img_list = generate_images()
+        img_directory, p1_guessed_faces, p2_guessed_faces, p1_selected_face, p2_selected_face = generate_images(p1_selected_face, p2_selected_face)
         new_board = False
+        current_player = random.choice([1, 2])
     
     if restart_screen == True:
         black_screen=pygame.draw.rect(screen, black, [0, 0, WIDTH, HEIGHT], 0) 
@@ -176,11 +236,10 @@ while running:
                     start_text = title_font.render("Start", True, white)
                     screen.blit(start_text, (580, 350))
                     pygame.display.flip()
-                    
         
     else:
-        restart_button, ask_button, guess_button, toggle_names_button = draw_backgrounds()
-        draw_board(rows, cols, img_directory, img_list, first_generation, toggle_names_condition)
+        restart_button, ask_button, guess_button, toggle_names_button = draw_backgrounds(current_player, toggle_names_condition, ask_condition, guess_condition)
+        draw_board(rows, cols, img_directory, p1_guessed_faces, first_generation, toggle_names_condition, current_player)
         first_generation = False
 
         for event in pygame.event.get():
@@ -188,41 +247,32 @@ while running:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if toggle_names_button.collidepoint(event.pos):
-                    if (toggle_names_condition == True):
+                    if toggle_names_condition == True:
                         toggle_names_condition = False
-                        toggle_names_button = toggle_names_button_creation(toggle_names_condition)
                     else:
                         toggle_names_condition = True
                 if restart_button.collidepoint(event.pos):
-                    first_generation = True
-                    new_board = True
-                    p1_selected_face = ""
-                    p2_selected_face = ""
-                    ###
-                    restart_button = pygame.draw.rect(screen, dark_gray, [30, HEIGHT-90, 220, 75], 0, 8)
-                    title_text = title_font.render("Restart", True, white)
-                    screen.blit(title_text, (40, 670))
-                    pygame.display.flip()
-                    pygame.time.wait(500)
-                    black_screen=pygame.draw.rect(screen, black, [0, 100, WIDTH, HEIGHT], 0) 
-                    title_text = title_font.render("Restarting", True, red)
-                    screen.blit(title_text, (500, 350))
-                    pygame.display.flip()
-                    pygame.time.wait(500)
-                    period1 = title_font.render(".", True, red)
-                    screen.blit(period1, (815, 350))
-                    pygame.display.flip()
-                    pygame.time.wait(1000)
-                    period2 = title_font.render(".", True, red)
-                    screen.blit(period1, (835, 350))
-                    pygame.display.flip()
-                    pygame.time.wait(1000)
-                    period3 = title_font.render(".", True, red)
-                    screen.blit(period3, (855, 350))
-                    pygame.display.flip()
-                    pygame.time.wait(1000)
-    
-    pygame.display.flip()
+                    first_generation, new_board = restart_button_logic(first_generation, new_board)
+                if ask_button.collidepoint(event.pos):
+                    if ask_condition == True:
+                        ask_condition = False
+                        if current_player == 1:
+                            current_player = 2
+                        else:
+                            current_player = 1
+                    else:
+                        ask_condition = True
+                if guess_button.collidepoint(event.pos):
+                    if guess_condition == True:
+                        guess_condition = False
+                        if current_player == 1:
+                            current_player = 2
+                        else:
+                            current_player = 1
+                    else:
+                        guess_condition = True
+
+    pygame.display.flip() #may not be needed
 pygame.quit()
 
 #THINGS TO DO:
